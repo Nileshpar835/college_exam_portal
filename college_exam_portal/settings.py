0f053f323c2713728577bcbb176ac5254bee7ca6
Nilesh Parmar <107906213+Nileshpar835@users.noreply.github.com>
2025-08-17 15:19:54 +0530

    Update settings.py

--- college_exam_portal/settings.py
+++ college_exam_portal/settings.py
@@ -64,6 +64,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'users.middleware.AccessDeniedMiddleware',  # Add this line
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
